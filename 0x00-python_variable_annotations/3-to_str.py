b5231c76dc084abdbb4cbf958f0683114ef87cef
GaviriaAmaya <judamayaga@unal.edu.co>
2020-05-04 23:30:38 -0500

    [Feature] Type anotated to_str function
    Takes a float and converts it to a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"Type anotated to string function"
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float number and returns as a string
+    floor(6.16) = '6.16'
+    """
+    return str(n)
