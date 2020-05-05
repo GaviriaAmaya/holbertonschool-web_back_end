019d0a8bf7045b64c8b7b795d60bd057dea9984e
GaviriaAmaya <judamayaga@unal.edu.co>
2020-05-05 00:22:34 -0500

    [Feature] Add a sum_mixed_list function
    Takes an int and float list and returns the sum of the list. Type anotated

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"Multiple type anotations on a list"
+from typing import List
+mixed_list = List[float or int]
+
+
+def sum_mixed_list(mxd_lst: mixed_list) -> float:
+    """
+    Takes a mixed list and returns the sum of the list
+    sum_mixed_list([4.1, 2.6, 1]) = 7.7
+    """
+    result: float = 0
+
+    for num in mxd_lst:
+        result = result + num
+
+    return result
