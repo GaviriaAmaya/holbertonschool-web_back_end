38f90137ca3bd219c0b58959c51ca7db6616e843
GaviriaAmaya <judamayaga@unal.edu.co>
2020-05-04 23:05:46 -0500

    [Feature] String concat function
    Strictly typed concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"Function that concatenates two strings"
+
+
+def concat(str1: str, str2: str) -> str:
+    "Strictly string concatenation: 'string' + 'another' = 'stringanother'"
+    return str1 + str2
