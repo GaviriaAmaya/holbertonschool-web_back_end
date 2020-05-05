a218eac6c7dafc90e60c1141718af08b062bf3d2
GaviriaAmaya <judamayaga@unal.edu.co>
2020-05-04 23:20:00 -0500

    [Feature] Type anotated floor function
    Takes a float number and rounds it to the floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"Type anotated floor function"
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Takes a float number, rounds it to the floor and returns an int
+    floor(6.16) = 6
+    """
+    return math.floor(n)
