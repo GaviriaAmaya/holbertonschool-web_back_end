51f54d43f789deb4ba24d82eb14abe613930cc7f
GaviriaAmaya <judamayaga@unal.edu.co>
2020-12-11 22:45:49 -0500

    [Add] Function to modify an array

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const arr = array;
-  for (const [idx, item] of arr.entries()) {
-    arr[idx] = appendString + item;
+  let arr = array;
+  for (let element of arr) {
+    element = appendString + element;
   }
-
   return arr;
 }
